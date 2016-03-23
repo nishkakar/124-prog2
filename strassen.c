1191daa21d6e42b87afff9421d308b4dd8a921cb
Nishant Kakar <nishant@neurotrack.com>
2016-03-23 16:44:59 -0400

    added test file and created construct_matrix function

--- strassen.c
+++ strassen.c
@@ -10,11 +10,44 @@
 #include <math.h>
 #include <stdbool.h>
 
+// for seeking
+#include <sys/types.h>
+#include <unistd.h>
+
+
+float** construct_matrix(int dimension, FILE* fp) {
+    float** matrix = malloc(dimension * sizeof(int*));
+    char buf[256];
+    for (int i = 0; i < dimension; i++) {
+        matrix[i] = malloc(dimension * sizeof(int));
+        for (int j = 0; j < dimension; j++) {
+            fgets(buf, sizeof(buf), fp);
+            matrix[i][j] = atoi(buf);
+            printf("%.0f ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+    return matrix;
+}
+
+// void print_graph(float** matrix, int dimension) {
+//     // prints out adjacency matrix
+//     for (int i = 0; i < dimension; ++i) {
+//         for (int j = 0; j < dimension; ++j) {
+//             printf("%f", matrix[i][j]);
+//         }
+//         printf("\n");
+//     }
+// }
+
 int main(int argc, char *argv[]) {
     int dimension = atoi(argv[2]);
     char* inputfile = argv[3];
 
-    printf("%s %s %s %s\n", argv[0], argv[1], argv[2], argv[3]);
+    FILE* fp;
+    fp = fopen(inputfile, "r");
+    float** matrix_A = construct_matrix(dimension, fp);
+    float** matrix_B = construct_matrix(dimension, fp);
 
     return 0;
 }
