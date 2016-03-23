64e82863b7613461edfd9c14e4bf2c27e8b30ba9
Nishant Kakar <nishant@neurotrack.com>
2016-03-22 22:02:52 -0400

    made strassen.c file skeleton

---
+++ strassen.c
@@ -0,0 +1,19 @@
+// ===============================
+// CS124 Programming Assignment 2
+// Ajay Nathan & Nishant Kakar
+// ===============================
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <math.h>
+#include <stdbool.h>
+
+int main(int argc, char *argv[]) {
+    int dimension = atoi(argv[2]);
+    int inputfile = atoi(argv[3]);
+
+    printf("%s %s %s %s\n", argv[0], argv[1], argv[2], argv[3]);
+
+}
