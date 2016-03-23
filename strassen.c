a1ca414db06c652ddd259dc8cc715cad2260d1cc
Nishant Kakar <nishant@neurotrack.com>
2016-03-23 17:08:14 -0400

    timed strassen runsfor each possible crossover point and printed product_matrix

--- strassen.c
+++ strassen.c
@@ -30,15 +30,19 @@ float** construct_matrix(int dimension, FILE* fp) {
     return matrix;
 }
 
-// void print_graph(float** matrix, int dimension) {
-//     // prints out adjacency matrix
-//     for (int i = 0; i < dimension; ++i) {
-//         for (int j = 0; j < dimension; ++j) {
-//             printf("%f", matrix[i][j]);
-//         }
-//         printf("\n");
-//     }
-// }
+void print_graph(float** matrix, int dimension) {
+    // prints out adjacency matrix
+    for (int i = 0; i < dimension; ++i) {
+        for (int j = 0; j < dimension; ++j) {
+            printf("%.0f ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+float** strassen(float** matrix_A, float** matrix_B, int dimension, int crossover_dimension) {
+    return matrix_A;
+}
 
 int main(int argc, char *argv[]) {
     int dimension = atoi(argv[2]);
@@ -48,6 +52,29 @@ int main(int argc, char *argv[]) {
     fp = fopen(inputfile, "r");
     float** matrix_A = construct_matrix(dimension, fp);
     float** matrix_B = construct_matrix(dimension, fp);
+
+    // times the calculation for all possible crossover points
+    time_t t;
+    int crossover_dimension = 1;
+    int optimal_dimension = -1;
+    int best_time = 10E6;
+    int total_time;
+    while (crossover_dimension < dimension) {
+        clock_t start = clock();
+        float** product_matrix = strassen(matrix_A, matrix_B, dimension, crossover_dimension);
+        total_time = (float) (clock() - start) / CLOCKS_PER_SEC;
+
+        if (crossover_dimension == 1) {
+            print_graph(product_matrix, dimension);
+        }
+
+        // if this was a faster calculation, update our records
+        if (total_time < best_time) {
+            optimal_dimension = crossover_dimension;
+            best_time = total_time;
+        }
+        crossover_dimension++;
+    }
 
     return 0;
 }
